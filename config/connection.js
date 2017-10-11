93c41e81f2014932f35f7f1bcabe949de617e286
Jacob Thomas <jcthomas00@gmail.com>
2017-10-10 19:41:52 -0500

    added heroky jawsdb mysql database

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 //file containing all database connection details 
 
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-	port: 3306,
-	host: "localhost",
-	user: "root",
-	password: "madonna",
-	database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: "localhost",
+		user: "root",
+		password: "****",
+		database: "burgers_db"
+	});
+}
 //Make sure DB connection can be initiated
 connection.connect((err)=>{
 	if(err){
